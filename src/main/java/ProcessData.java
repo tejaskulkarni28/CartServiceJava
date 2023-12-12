a2a1df652b13ce1054c15c7011688f3d131a6fc1
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2023-12-13 00:08:52 +0530

    Commited

--- src/main/java/ProcessData.java
+++ src/main/java/ProcessData.java
@@ -1,11 +1,11 @@
 
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class ProcessData
@@ -34,7 +34,13 @@ public class ProcessData extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		doGet(request, response);
+		String[] values = request.getParameterValues("cartValue[]");
+		PrintWriter out = response.getWriter();
+		for(String value:values) {
+			out.println(value);
+		}
+		
 	}
 
 }
